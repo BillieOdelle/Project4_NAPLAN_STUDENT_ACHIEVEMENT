048026453ee783f1583aea9cec0b5d8b912dd38c
BillieOdelle <billie@bogore.com>
2023-08-07 18:10:14 +1000

    committing files worked on today

--- Resources/naplan_user.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM naplan_results_clean
-
-SELECT subgroup FROM Naplan_results_clean
-
-GRANT pg_read_all_data, pg_write_all_data TO naplan_user;
-
-COMMENT ON ROLE naplan_user IS 'password is password';
